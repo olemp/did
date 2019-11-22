424dce39234555620edfe10ff48c99e77d983aa2
olemp <olemp@puzzlepart.com>
2019-11-22 12:01:21 +0100

    TimeEntriesController

--- Did365App/API/TimeEntriesController.cs
+++ Did365App/API/TimeEntriesController.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using Did365App.Services;
+using Microsoft.WindowsAzure.Storage.Table;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web.Http;
 
 namespace Did365App.API
@@ -11,9 +14,12 @@ namespace Did365App.API
     {
         [Authorize]
         [HttpGet]
-        public IEnumerable<string> Get()
+        public async Task<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            using (TableService tableService = new TableService("TimeEntries"))
+            {
+                return await tableService.Add();
+            }
         }
     }
 }
--- Did365App/Models/TimeEntry.cs
+++ Did365App/Models/TimeEntry.cs
@@ -1,11 +1,20 @@
-﻿using System;
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
 namespace Did365App.Models
 {
-    public class TimeEntry
+    public class TimeEntry :TableEntity
     {
+        public string Subject { get; set; }
+
+        public TimeEntry()
+        {
+            this.PartitionKey = "Default";
+            this.RowKey = Guid.NewGuid().ToString();
+            this.Subject = Guid.NewGuid().ToString();
+        }
     }
 }
