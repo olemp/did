18bfdd1180766b7e7b8ffbb79ce5786ad84ebc21
olemp <olemp@puzzlepart.com>
2019-11-22 08:52:46 +0100

    App

---
+++ Did365App/App_Start/Startup.Auth.cs
@@ -0,0 +1,16 @@
+﻿using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Did365App
+{
+    public partial class Startup
+
+    {
+        public void ConfigureAuth(IAppBuilder app)
+        {
+        }
+    }
+}
---
+++ Did365App/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Did365App.Startup))]
+
+namespace Did365App
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
