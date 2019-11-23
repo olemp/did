89bf3ff4b82d03e362c8735bf2b12ed207546857
olemp <olemp@puzzlepart.com>
2019-11-23 13:42:03 +0100

    Added tokens

--- did365node/server/routes/auth.js
+++ did365node/server/routes/auth.js
@@ -4,43 +4,45 @@ const router = express.Router();
 
 /* GET auth callback. */
 router.get('/signin',
-  function  (req, res, next) {
-    passport.authenticate('azuread-openidconnect',
-      {
-        response: res,
-        prompt: 'login',
-        failureRedirect: '/',
-        failureFlash: true
-      }
-    )(req,res,next);
-  },
-  function(req, res) {
-    res.redirect('/');
-  }
+    function (req, res, next) {
+        console.log('router', './signin');
+        passport.authenticate('azuread-openidconnect',
+            {
+                response: res,
+                prompt: 'login',
+                failureRedirect: '/',
+                failureFlash: true
+            }
+        )(req, res, next);
+    },
+    function (req, res) {
+        res.redirect('/');
+    }
 );
 
 router.post('/callback',
-  function(req, res, next) {
-    passport.authenticate('azuread-openidconnect',
-      {
-        response: res,
-        failureRedirect: '/',
-        failureFlash: true
-      }
-    )(req,res,next);
-  },
-  function(req, res) {
-    res.redirect('/');
-  }
+    function (req, res, next) {
+        console.log('router', './callback');
+        passport.authenticate('azuread-openidconnect',
+            {
+                response: res,
+                failureRedirect: '/',
+                failureFlash: true
+            }
+        )(req, res, next);
+    },
+    function (req, res) {
+        res.redirect('/');
+    }
 );
 
 router.get('/signout',
-  function(req, res) {
-    req.session.destroy(function(err) {
-      req.logout();
-      res.redirect('/');
-    });
-  }
+    function (req, res) {
+        req.session.destroy(function (err) {
+            req.logout();
+            res.redirect('/');
+        });
+    }
 );
 
 module.exports = router;
