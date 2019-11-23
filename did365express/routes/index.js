0aa0a283b73da4dd968d6040dd3deb2328f4fe0a
olemp <olemp@puzzlepart.com>
2019-11-23 17:17:22 +0100

    week_view (needs api)

--- did365express/routes/index.js
+++ did365express/routes/index.js
@@ -1,13 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   let params = {
     active: { home: true }
   };
 
   res.render('index', params);
+});
+
+router.get('/week_view', function (req, res, next) {
+  res.render('week_view');
 });
 
 module.exports = router;
