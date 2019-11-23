5054ff57f5ba24d57be0a67fa4049bed7208c080
olemp <olemp@puzzlepart.com>
2019-11-23 13:31:37 +0100

    Initial auth (https://github.com/microsoftgraph/msgraph-training-nodeexpressapp/tree/master/Demos/03-add-msgraph/graph-tutorial)

--- did365node/server/index.js
+++ did365node/server/index.js
@@ -1,14 +1,107 @@
 /* eslint consistent-return:0 */
-
+require('dotenv').config();
 const express = require('express');
 const { resolve } = require('path');
 const logger = require('./util//logger');
-
 const argv = require('./util/argv');
 const port = require('./util//port');
 const setup = require('./middlewares/frontendMiddleware');
+const session = require('express-session');
+const flash = require('connect-flash');
+const passport = require('passport');
+const OIDCStrategy = require('passport-azure-ad').OIDCStrategy;
+const graph = require('./graph');
+
+// Configure simple-oauth2
+const oauth2 = require('simple-oauth2').create({
+  client: {
+    id: process.env.OAUTH_APP_ID,
+    secret: process.env.OAUTH_APP_PASSWORD
+  },
+  auth: {
+    tokenHost: process.env.OAUTH_AUTHORITY,
+    authorizePath: process.env.OAUTH_AUTHORIZE_ENDPOINT,
+    tokenPath: process.env.OAUTH_TOKEN_ENDPOINT
+  }
+});
+
+var users = {};
+
+passport.serializeUser(function(user, done) {
+  users[user.profile.oid] = user;
+  done (null, user.profile.oid);
+});
+
+passport.deserializeUser(function(id, done) {
+  done(null, users[id]);
+});
+
+// Callback function called once the sign-in is complete
+// and an access token has been obtained
+async function signInComplete(iss, sub, profile, accessToken, refreshToken, params, done) {
+  if (!profile.oid) {
+    return done(new Error("No OID found in user profile."), null);
+  }
+
+  try{
+    const user = await graph.getUserDetails(accessToken);
+
+    if (user) {
+      // Add properties to profile
+      profile['email'] = user.mail ? user.mail : user.userPrincipalName;
+    }
+  } catch (err) {
+    done(err, null);
+  }
+
+  // Create a simple-oauth2 token from raw tokens
+  let oauthToken = oauth2.accessToken.create(params);
+
+  // Save the profile and tokens in user storage
+  users[profile.oid] = { profile, oauthToken };
+  return done(null, users[profile.oid]);
+}
+
+// Configure OIDC strategy
+passport.use(new OIDCStrategy(
+  {
+    identityMetadata: `${process.env.OAUTH_AUTHORITY}${process.env.OAUTH_ID_METADATA}`,
+    clientID: process.env.OAUTH_APP_ID,
+    responseType: 'code id_token',
+    responseMode: 'form_post',
+    redirectUrl: process.env.OAUTH_REDIRECT_URI,
+    allowHttpForRedirectUrl: true,
+    clientSecret: process.env.OAUTH_APP_PASSWORD,
+    validateIssuer: false,
+    passReqToCallback: false,
+    scope: process.env.OAUTH_SCOPES.split(' ')
+  },
+  signInComplete
+));
+
+
 
 const app = express();
+
+// Session middleware
+// NOTE: Uses default in-memory session store, which is not
+// suitable for production
+app.use(session({
+  secret: 'your_secret_value_here',
+  resave: false,
+  saveUninitialized: false,
+  unset: 'destroy'
+}));
+
+app.use(flash());
+
+
+// Initialize passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+app.use('/auth', require('./routes/auth'));
 
 // If you need a backend, e.g. an API, add your custom backend-specific middleware here
 // app.use('/api', myApi);
