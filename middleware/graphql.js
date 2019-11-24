6dc184cc668fc49b0834fe4cff1a156f2a01230b
olemp <olemp@puzzlepart.com>
2019-11-25 00:52:33 +0100

    graphql

---
+++ middleware/graphql.js
@@ -0,0 +1,36 @@
+const path = require("path");
+const graphql = require("express-graphql");
+const { importSchema } = require("graphql-import");
+const { makeExecutableSchema } = require("graphql-tools");
+const { query, parseArray } = require("../services/table");
+const { TableQuery } = require("azure-storage");
+
+const typeDefs = importSchema(path.join(__dirname, "./schema.graphql"));
+const resolvers = {
+  Query: {
+    projects: async () => {
+      const result = await query(
+        process.env.AZURE_STORAGE_PROJECTS_TABLE_NAME,
+        new TableQuery().top(10).select("CustomerKey", "ProjectKey", "Name")
+      );
+      return parseArray(result);
+    },
+    customers: async () => {
+      const result = await query(
+        process.env.AZURE_STORAGE_CUSTOMERS_TABLE_NAME,
+        new TableQuery().top(10).select("CustomerKey", "Name")
+      );
+      return parseArray(result);
+    }
+  }
+};
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers
+});
+
+module.exports = graphql({
+  schema: schema,
+  rootValue: root,
+  graphiql: true
+});
