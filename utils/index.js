4406c92452b478c79e7716ff90dc439992b0725b
olemp <olemp@puzzlepart.com>
2019-11-25 18:37:03 +0100

    Added util getYear

--- utils/index.js
+++ utils/index.js
@@ -1,9 +1,10 @@
 const moment = require('moment');
 
-function getDurationMinutes(startTime, endTime) {
-    return moment.duration(moment(endTime).diff(moment(startTime))).asMinutes()
-}
-
-module.export = {
-    getDurationMinutes,
+module.exports = {
+    getDurationMinutes: (startTime, endTime) => {
+        return moment.duration(moment(endTime).diff(moment(startTime))).asMinutes()
+    },
+    getYear: () => {
+        return moment().year();
+    }
 }
