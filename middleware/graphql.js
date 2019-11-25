481306bbde5190f4e58f5ae15a86f23261c7456f
olemp <olemp@puzzlepart.com>
2019-11-25 11:45:58 +0100

    Added comments to graphql middleware

--- middleware/graphql.js
+++ middleware/graphql.js
@@ -10,16 +10,25 @@ const moment = require('moment');
 const typeDefs = importSchema(path.join(__dirname, './schema.graphql'));
 const resolvers = {
   Query: {
+    /**
+     * @resolver projects
+     */
     projects: async () => {
       const result = await query(process.env.AZURE_STORAGE_PROJECTS_TABLE_NAME, new TableQuery().top(1000).select('CustomerKey', 'ProjectKey', 'Name'));
       return parseArray(result).map(r => ({ ...r, key: `${r.customerKey} ${r.projectKey}` }));;;
     },
 
+    /**
+     * @resolver customers
+     */
     customers: async () => {
       const result = await query(process.env.AZURE_STORAGE_CUSTOMERS_TABLE_NAME, new TableQuery().top(10).select('CustomerKey', 'Name'));
       return parseArray(result).map(r => ({ ...r, key: r.customerKey }));;
     },
 
+    /**
+     * @resolver weekView
+     */
     weekView: async (_obj, args, { user, isAuthenticated }) => {
       if (!isAuthenticated) return [];
       const calendarView = await graph.getCalendarView(user.oauthToken.access_token, args.startOfWeek);
@@ -50,6 +59,9 @@ const resolvers = {
       return events;
     },
 
+    /**
+     * @resolver customerProjects
+     */
     customerProjects: async (_obj, args, { isAuthenticated }) => {
       if (!isAuthenticated) return [];
       const result = await query(process.env.AZURE_STORAGE_PROJECTS_TABLE_NAME, new TableQuery().top(50).where('CustomerKey eq ?', args.customerKey).select('CustomerKey', 'ProjectKey', 'Name'));
@@ -57,6 +69,9 @@ const resolvers = {
       return projects;
     },
 
+    /**
+     * @resolver approvedEntries
+     */
     approvedEntries: async (_obj, args, { isAuthenticated }) => {
       if (!isAuthenticated) return [];
       const result = await query(process.env.AZURE_STORAGE_APPROVEDTIMEENTRIES_TABLE_NAME, new TableQuery().top(50).where('ProjectKey eq ?', args.projectKey));
