26f0f892981583ccda521cb414f6e53dc661d6d1
olemp <olemp@puzzlepart.com>
2019-11-25 18:05:28 +0100

    Fixed typo in webpack.config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -22,7 +22,7 @@ module.exports = {
     new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
   ],
   output: {
-    path: path.resolve(__dirname, '../js/scripts'),
+    path: path.resolve(__dirname, '../public/js'),
     filename: 'did365.bundle.js'
   }
 };
