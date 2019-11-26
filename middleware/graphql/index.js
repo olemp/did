f52609eab550afd1580c7078a1bc9bb60e7c23f4
Kim Damsleth <kim@damsleth.com>
2019-11-26 17:20:34 +0100

    deprecated rootValue root -> global, plus graphql typo

--- middleware/graphql/index.js
+++ middleware/graphql/index.js
@@ -26,8 +26,8 @@ const schema = makeExecutableSchema({
 
 module.exports = graphql((req) => ({
   schema: schema,
-  rootValue: root,
-  graphiql: req.app.get('env') === 'development',
+  rootValue: global,
+  graphql: req.app.get('env') === 'development',
   context: {
     user: req.user,
     tid: req.user.profile._json.tid,
