bde2c852402e852a749d12f746a53245b4074255
olemp <olemp@puzzlepart.com>
2019-11-26 17:38:13 +0100

    weekView graphql: Separated matchProject into new function

--- middleware/graphql/index.js
+++ middleware/graphql/index.js
@@ -2,26 +2,10 @@ const path = require('path');
 const graphql = require('express-graphql');
 const { importSchema } = require('graphql-import');
 const { makeExecutableSchema } = require('graphql-tools');
-const resolvers = require('./resolvers');
 
 const schema = makeExecutableSchema({
   typeDefs: importSchema(path.join(__dirname, './schema.graphql')),
-  resolvers: {
-    Query: {
-      confirmedEntries: resolvers.confirmedEntries,
-      customerProjects: resolvers.customerProjects,
-      customers: resolvers.customers,
-      projects: resolvers.projects,
-      weekView: resolvers.weekView,
-      isWeekConfirmed: resolvers.isWeekConfirmed,
-      confirmedHours: resolvers.confirmedHours,
-      confirmedMinutes: resolvers.confirmedMinutes,
-    },
-    Mutation: {
-      confirmWeek: resolvers.confirmWeek,
-      unconfirmWeek: resolvers.unconfirmWeek,
-    }
-  },
+  resolvers: require('./resolvers'),
 });
 
 module.exports = graphql((req) => ({
