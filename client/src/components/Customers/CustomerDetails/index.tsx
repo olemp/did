f4ef07b011917b50ff907758805cecbffe934b17
olemp <olemp@puzzlepart.com>
2019-11-26 23:18:21 +0100

    Added apollo libs

--- client/src/components/Customers/CustomerDetails/index.tsx
+++ client/src/components/Customers/CustomerDetails/index.tsx
@@ -1,17 +1,39 @@
 import * as React from 'react';
-import { ICustomerDetailsProps } from './IProjectDetailsProps';
 import { ProjectList } from '../../Projects/ProjectList';
+import { useQuery } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
+import { MessageBar, MessageBarType } from 'office-ui-fabric-react/lib/MessageBar';
+import { Spinner } from 'office-ui-fabric-react/lib/Spinner';
 
-export const CustomerDetails = ({ customer, projects }: ICustomerDetailsProps) => (
-    <div>
-        <h2>{customer.name}</h2>
-        <p hidden={!customer.description}>{customer.description}</p>
-        <p hidden={!customer.webLink}><a href={customer.webLink}>{customer.webLink}</a></p>
-        <ProjectList
-            projects={projects}
-            search={{ placeholder: `Search projects for ${customer.name}...` }}
-            renderLink={true}
-            selection={null}
-            height={300} />
-    </div>
-);
+const GET_PROJECTS = gql`
+    query($customerKey: String!) {
+        customerProjects(customerKey: $customerKey) {
+            key,
+            customerKey,
+            name
+        }
+    }
+`;
+
+export const CustomerDetails = ({ customer }) => {
+    if (!customer) return null;
+    const { loading, error, data } = useQuery(GET_PROJECTS, { variables: { customerKey: customer.key } });
+    if (loading) {
+        return <Spinner label='Loading...' />;
+    }
+    if (error) {
+        return <MessageBar messageBarType={MessageBarType.error}>An error occured.</MessageBar>;
+    }
+    return (
+        <div>
+            <h2>{customer.name}</h2>
+            <p hidden={!customer.description}>{customer.description}</p>
+            <p hidden={!customer.webLink}><a href={customer.webLink}>{customer.webLink}</a></p>
+            <ProjectList
+                projects={data.customerProjects}
+                search={{ placeholder: `Search projects for ${customer.name}...` }}
+                renderLink={true}
+                height={300} />
+        </div>
+    );
+};
