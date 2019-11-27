342cf866c1e3d3796ad591c45bf2607beff8b484
olemp <olemp@puzzlepart.com>
2019-11-27 12:08:07 +0100

    Attempting to fix event is not defined bug

--- middleware/graphql/resolvers/weekView.js
+++ middleware/graphql/resolvers/weekView.js
@@ -4,11 +4,11 @@ const graph = require('../../../services/graph');
 /**
  * Checks for project match in event
  * 
- * @param {*} event 
+ * @param {*} evt 
  * @param {*} projectKey 
  */
-function matchProject(event, projectKey) {
-    let content = [event.title, event.body, JSON.stringify(event.categories)].join(' ').toUpperCase();
+function matchProject(evt, projectKey) {
+    let content = [evt.title, evt.body, JSON.stringify(evt.categories)].join(' ').toUpperCase();
     return content.indexOf(projectKey.toUpperCase()) !== -1;
 }
 
@@ -17,11 +17,11 @@ module.exports = async (_obj, args, context) => {
     const calendarView = await graph.getCalendarView(context.user.oauthToken.access_token, args.weekNumber);
     const result = await queryTable(process.env.AZURE_STORAGE_PROJECTS_TABLE_NAME, new TableQuery().top(1000).where('PartitionKey eq ?', context.user.profile._json.tid).select('CustomerKey', 'ProjectKey', 'Name'));
     const projects = parseArray(result).map(r => ({ ...r, key: `${r.customerKey} ${r.projectKey}` }));
-    const events = calendarView.map(event => {
-        let project = projects.filter(p => matchProject(event, p.key))[0];
-        return { ...event, project };
+    const events = calendarView.map(evt => {
+        let project = projects.filter(p => matchProject(evt, p.key))[0];
+        return { ...evt, project };
     });
-    const totalDuration = events.reduce((sum, event) => sum + event.durationMinutes, 0);
-    const matchedDuration = events.filter(event => event.project).reduce((sum, event) => sum + event.durationMinutes, 0);
+    const totalDuration = events.reduce((sum, evt) => sum + evt.durationMinutes, 0);
+    const matchedDuration = events.filter(evt => evt.project).reduce((sum, evt) => sum + evt.durationMinutes, 0);
     return { events, totalDuration, matchedDuration };
 };
