919ab836042d0a7ff27ab0c03dbeecf68c4478b7
Ole Martin Pettersen <olemp@puzzlepart.com>
2019-11-28 16:11:50 +0000

    Revert "Feature/user roles (pull request #56)"
    This reverts pull request #56.
    
    > ‌
    > 
    > * `Users.csv` for azure table storage import
    > * Added env variable `AZURE_STORAGE_USERS_TABLE_NAME` to `.env.sample`
    > 
    >     * Web App is updated with the new setting
    >     
    > * Partial to show user role
    > * Simplified routes/auth
    > * Added `getUser`to table service
    > * Ignoring `server.js` on root \(we are using `/bin/www`\)
    > * Saving `role` and `isAdmin` to hbs context
    > 
    >     * A user is admin if he’s invoice manager or admin, this might change
    >     
    > * Retrieving user data in `passport.deserializeUser`
    > 
    > ‌

--- services/table.js
+++ services/table.js
@@ -2,50 +2,6 @@ const { createTableService, TableQuery, TableUtilities } = require('azure-storag
 const azureTableService = createTableService(process.env.AZURE_STORAGE_CONNECTION_STRING);
 
 /**
- * Parse an array of azure table storage entities
- * 
- * Makes the keys camelCase and adds RowKey as 'id'
- * 
- * Also skips PartitionKey
- * 
- * @param {*} arr The array of entities to parse
- */
-function parseArray(arr) {
-    return arr.map(item => Object.keys(item)
-        .filter(key => key !== 'PartitionKey')
-        .reduce((obj, key) => {
-            const camelCaseKey = key.charAt(0).toLowerCase() + key.slice(1);
-            const value = item[key]._;
-            if (key === 'RowKey') {
-                obj.id = value;
-                return obj;
-            }
-            switch (item[key].$) {
-                case 'Edm.DateTime': {
-                    obj[camelCaseKey] = value.toISOString();
-                }
-                    break;
-                default: {
-                    obj[camelCaseKey] = value;
-                }
-            }
-            return obj;
-        }, {}));
-}
-
-/**
- * Function that simplifes creating a new TableQuery from azure-storage
- * 
- * @param {*} top 
- * @param {*} select 
- */
-function createQuery(top, select) {
-    let query = new TableQuery().top(top);
-    if (select) query = query.select(select);
-    return query;
-}
-
-/**
  * Queries a table using the specified query
  * 
  * @param {*} table 
@@ -113,17 +69,35 @@ function getSubscription(tenantId) {
 };
 
 /**
- * Get user
+ * Parse an array of azure table storage entities
  * 
- * @param {*} tenantId 
- * @param {*} userId 
+ * Makes the keys camelCase and adds RowKey as 'id'
+ * 
+ * Also skips PartitionKey
+ * 
+ * @param {*} arr The array of entities to parse
  */
-async function getUser(tenantId, userId) {
-    let filter = TableQuery.stringFilter('PartitionKey', TableUtilities.QueryComparisons.EQUAL, tenantId);
-    filter = TableQuery.combineFilters(filter, TableUtilities.TableOperators.AND, TableQuery.stringFilter('RowKey', TableUtilities.QueryComparisons.EQUAL, userId));
-    const query = createQuery(1, ['Role', 'StartPage']).where(filter);
-    const users = await queryTable(process.env.AZURE_STORAGE_USERS_TABLE_NAME, query);
-    return parseArray(users)[0];
+function parseArray(arr) {
+    return arr.map(item => Object.keys(item)
+        .filter(key => key !== 'PartitionKey')
+        .reduce((obj, key) => {
+            const camelCaseKey = key.charAt(0).toLowerCase() + key.slice(1);
+            const value = item[key]._;
+            if (key === 'RowKey') {
+                obj.id = value;
+                return obj;
+            }
+            switch (item[key].$) {
+                case 'Edm.DateTime': {
+                    obj[camelCaseKey] = value.toISOString();
+                }
+                    break;
+                default: {
+                    obj[camelCaseKey] = value;
+                }
+            }
+            return obj;
+        }, {}));
 }
 
 module.exports = {
@@ -131,12 +105,15 @@ module.exports = {
     addEntity: addEntity,
     executeBatch: executeBatch,
     getSubscription: getSubscription,
-    getUser: getUser,
     parseArray: parseArray,
     isEqual: TableUtilities.QueryComparisons.EQUAL,
     and: TableUtilities.TableOperators.AND,
     combine: TableQuery.combineFilters,
     stringFilter: TableQuery.stringFilter,
     intFilter: TableQuery.int32Filter,
-    createQuery: createQuery,
+    createQuery: (top, select) => {
+        let query = new TableQuery().top(top);
+        if (select) query = query.select(select);
+        return query;
+    }
 }
