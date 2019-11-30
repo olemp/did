a117ca249753e6e4a3d8143727a99e2cd4409204
olemp <olemp@puzzlepart.com>
2019-11-30 16:32:27 +0100

    Fixed arguments usage in getProjects

--- middleware/graphql/resolvers/getProjects.js
+++ middleware/graphql/resolvers/getProjects.js
@@ -1,7 +1,14 @@
 const log = require('debug')('middleware/graphql/getProjects');
 
+/**
+ * Get projects
+ * 
+ * @param {*} _obj Unused object
+ * @param {*} args Args (customerKey, sortBy)
+ * @param {*} context Context
+ */
 async function getProjects(_obj, args, context) {
-    let projects = await context.services.storage.getProjects(args.customerId, args.sortBy);
+    let projects = await context.services.storage.getProjects(args.customerKey, args.sortBy);
     log('Retrieved %s projects from storage', projects.length);
     return projects;
 }
