212c1df1e05f77b380d17910942b7f0d826a730b
olemp <olemp@puzzlepart.com>
2019-11-30 13:01:22 +0100

    Updated getEvents to use StorageService

--- middleware/graphql/resolvers/getEvents.js
+++ middleware/graphql/resolvers/getEvents.js
@@ -1,8 +1,9 @@
-const { queryTable, parseArray, createQuery } = require('../../../services/table');
-const GraphService = require('../../../services/graph');
 const getConfirmedDuration = require('./getConfirmedDuration');
 const _ = require('underscore');
 const findBestMatch = require('string-similarity').findBestMatch;
+const StorageService = require('../../../services/storage');
+const GraphService = require('../../../services/graph');
+const debug = require('debug');
 
 
 /**
@@ -13,23 +14,33 @@ const findBestMatch = require('string-similarity').findBestMatch;
  * @param {*} projectKey 
  */
 function getProjectSuggestion(projects, customer, projectKey) {
+    debug('getEvents:getProjectSuggestion')('Finding best match for [%s]', projectKey);
     let customerProjects = projects.filter(p => p.customerKey === customer.key);
-    let projectKeys = customerProjects.map(p => p.projectKey.toUpperCase());
+    let projectKeys = customerProjects.map(p => p.projectKey);
     let sm = findBestMatch(projectKey, projectKeys);
     let target = (sm.bestMatch && sm.bestMatch.rating > 0) ? sm.bestMatch.target : null;
     if (!target) return null;
-    let suggestion = customerProjects.filter(p => p.projectKey.toUpperCase() === target.toUpperCase())[0];
+    let suggestion = customerProjects.filter(p => p.projectKey === target.toUpperCase())[0];
+    debug('getEvents:getProjectSuggestion')('Project [%s] is best match for [%s]', suggestion.projectKey, projectKey);
     return suggestion;
 }
 
 /**
  * Find match
  * 
+ * First looking in categories, then in
+ * 
  * @param {*} content 
+ * @param {*} categories 
  */
-function findMatch(content) {
-    const regex = /[\(\{\[]((?<customerKey>[A-Za-z0-9]*?)\s(?<projectKey>[A-Za-z0-9]*?))[\)\]\}]/gm;
-    const match = regex.exec(content);
+function findMatch(content, categories) {
+    let regex = /((?<customerKey>[A-Za-z0-9]*?)\s(?<projectKey>[A-Za-z0-9]*))/gm;
+    let match = regex.exec(categories);
+    if (match && match.groups) {
+        return match.groups;
+    }
+    regex = /[\(\{\[]((?<customerKey>[A-Za-z0-9]*?)\s(?<projectKey>[A-Za-z0-9]*?))[\)\]\}]/gm;
+    match = regex.exec(content);
     return match ? match.groups : null;
 }
 
@@ -41,18 +52,22 @@ function findMatch(content) {
  * @param {*} customers 
  */
 function matchEvent(evt, projects, customers) {
-    let categories = JSON.stringify(evt.categories).toUpperCase();
+    debug('getEvents:matchEvent')('Finding match for [%s]', evt.title);
+    let categories = evt.categories.join(' ').toUpperCase();
     let content = [evt.title, evt.body, categories].join(' ').toUpperCase();
-    let match = findMatch(content);
+    let match = findMatch(content, categories);
     if (match) {
+        debug('getEvents:matchEvent')('Found match for [%s]: %s', evt.title, JSON.stringify(match));
         evt.project = projects.filter(p => _.isMatch(p, match))[0];
         evt.customer = customers.filter(c => c.key === match.customerKey)[0];
     } else {
         evt.project = projects.filter(p => content.indexOf(p.key) !== -1)[0];
-        if (evt.project) evt.customer = customers.filter(c => c.key === evt.project.key.split(' ')[0])[0];
+        if (evt.project) {
+            evt.customer = customers.filter(c => c.key === evt.project.key.split(' ')[0])[0];
+        }
     }
-    if (evt.customer) {
-        evt.suggestedProject = !evt.project && getProjectSuggestion(projects, evt.customer, match.projectKey);
+    if (evt.customer && !evt.project) {
+        evt.suggestedProject = getProjectSuggestion(projects, evt.customer, match.projectKey);
     }
     return {
         ...evt,
@@ -62,15 +77,13 @@ function matchEvent(evt, projects, customers) {
 }
 
 async function getEvents(_obj, args, context) {
-    let events = await new GraphService(context.user.oauthToken.access_token).getEvents(args.weekNumber);
-    const query = createQuery(1000).where('PartitionKey eq ?', context.tid);
-    const [pRes, cRes, confirmedDuration] = await Promise.all([
-        queryTable(process.env.AZURE_STORAGE_PROJECTS_TABLE_NAME, query),
-        queryTable(process.env.AZURE_STORAGE_CUSTOMERS_TABLE_NAME, query),
+    debug('getEvents:main')('Retrieving events for week %s', args.weekNumber);
+    let [events, projects, customers, confirmedDuration] = await Promise.all([
+        new GraphService(context.user.oauthToken.access_token).getEvents(args.weekNumber),
+        new StorageService(context.tid).getProjects(),
+        new StorageService(context.tid).getCustomers(),
         getConfirmedDuration(null, { weekNumber: args.weekNumber, type: 'DurationMinutes' }, context),
     ])
-    const projects = parseArray(pRes).map(r => ({ ...r, key: `${r.customerKey} ${r.projectKey}`.toUpperCase() }));
-    const customers = parseArray(cRes).map(r => ({ ...r, key: r.customerKey.toUpperCase() }));;
     events = events.map(evt => matchEvent(evt, projects, customers));
     const totalDuration = events.reduce((sum, evt) => sum + evt.durationMinutes, 0);
     const matchedEvents = events.filter(evt => (evt.project && evt.project.id));
