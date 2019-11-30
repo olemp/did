cb6156d234d9ee9d0d8ab1f5d39aa9ca69f6f4e3
olemp <olemp@puzzlepart.com>
2019-11-30 16:25:34 +0100

    Fixed getConfirmedTimeEntries in middleware/graphql

--- middleware/graphql/resolvers/getConfirmedTimeEntries.js
+++ middleware/graphql/resolvers/getConfirmedTimeEntries.js
@@ -1,7 +1,14 @@
 const log = require('debug')('middleware/graphql/getConfirmedTimeEntries');
 
+/**
+ * Get confirmed time entries
+ * 
+ * @param {*} _obj Unused object
+ * @param {*} args Args (projectKey, resourceId)
+ * @param {*} context Context
+ */
 async function getConfirmedTimeEntries(_obj, args, context) {
-    const confirmedTimeEntries = await context.services.storage.getConfirmedTimeEntries(args.resourceId);
+    const confirmedTimeEntries = await context.services.storage.getConfirmedTimeEntries(args.resourceId, null, args.projectKey);
     log('Retrieved %s confirmed time entries from storage', confirmedTimeEntries.length);
     return confirmedTimeEntries;
 };
