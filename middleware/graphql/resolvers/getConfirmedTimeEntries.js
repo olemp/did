326d6f227f3cfc3c9ef9e4cd85ed0b540f131266
olemp <olemp@puzzlepart.com>
2019-11-30 16:22:54 +0100

    Added await to getConfirmedTimeEntries

--- middleware/graphql/resolvers/getConfirmedTimeEntries.js
+++ middleware/graphql/resolvers/getConfirmedTimeEntries.js
@@ -1,7 +1,7 @@
 const log = require('debug')('middleware/graphql/getConfirmedTimeEntries');
 
 async function getConfirmedTimeEntries(_obj, args, context) {
-    const confirmedTimeEntries = context.services.storage.getConfirmedTimeEntries(args.resourceId);
+    const confirmedTimeEntries = await context.services.storage.getConfirmedTimeEntries(args.resourceId);
     log('Retrieved %s confirmed time entries from storage', confirmedTimeEntries.length);
     return confirmedTimeEntries;
 };
