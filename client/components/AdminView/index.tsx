3fe7e54832eacf88b611434f1644da13d2726667
Ole Martin Pettersen <olemp@puzzlepart.com>
2019-12-02 06:48:02 +0000

    Merged in feature/weekstatuscolumn (pull request #95)
    WeekStatusColumn in separate file

--- client/components/AdminView/index.tsx
+++ client/components/AdminView/index.tsx
@@ -1,33 +1,16 @@
 
-import { useQuery, useMutation } from '@apollo/react-hooks';
+import { useQuery } from '@apollo/react-hooks';
 import { List } from 'components/List';
-import { Reports } from './Reports';
-import { Icon } from 'office-ui-fabric-react/lib/Icon';
 import { Pivot, PivotItem } from 'office-ui-fabric-react/lib/Pivot';
 import * as React from 'react';
-import { useState } from 'react';
 import { generateColumn } from 'utils/generateColumn';
 import GET_DATA from './GET_DATA';
-import UPDATE_WEEK from './UPDATE_WEEK';
-
-const WeekClosedColumn = ({ weekNumber, closed }) => {
-    const [updateWeek] = useMutation(UPDATE_WEEK);
-    const [isClosed, setIsClosed] = useState<boolean>(closed);
-
-    const onUpdateWeek = () => updateWeek({ variables: { weekNumber, closed: !isClosed } }).then(({ data: { closed } }) => setIsClosed(closed));
-
-    return (
-        <span title={isClosed ? 'The week is closed by an administrator. Click to open it.' : 'The week is open for everyone. Click to close it.'}>
-            <a href='#' onClick={onUpdateWeek}>
-                <Icon iconName={isClosed ? 'LockSolid' : 'OpenFile'} />
-            </a>
-        </span>
-    );
-}
+import { Reports } from './Reports';
+import { WeekStatusColumn } from './WeekStatusColumn';
 
 const WEEK_LIST_COLUMNS = [
     generateColumn('id', 'Week number', { maxWidth: 150 }),
-    generateColumn('closed', undefined, undefined, (week: any) => <WeekClosedColumn weekNumber={parseInt(week.id)} closed={week.closed} />)
+    generateColumn('closed', undefined, undefined, (week: any) => <WeekStatusColumn weekNumber={parseInt(week.id)} closed={week.closed} />)
 ];
 
 const USER_LIST_COLUMNS = [
