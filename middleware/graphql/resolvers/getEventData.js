0e6e563cc38e95f74af029f7e9ed1d2485dabec3
Ole Martin Pettersen <olemp@puzzlepart.com>
2019-12-04 14:26:56 +0000

    Merged in feature/getprojectsuggestion-verbose-logging (pull request #126)
    More verbose logging in getProjectSuggestion

--- middleware/graphql/resolvers/getEventData.js
+++ middleware/graphql/resolvers/getEventData.js
@@ -11,17 +11,20 @@ const log = require('debug')('middleware/graphql/getEventData');
  */
 function getProjectSuggestion(projects, customer, projectKey) {
     try {
-        log('Finding best match for [%s]', projectKey);
+        log('(getProjectSuggestion) Finding best match for [%s]', projectKey);
         let customerProjects = projects.filter(p => p.customerKey === customer.id);
+        log('(getProjectSuggestion) Found [%s] projects for customer [%s]', customer.id);
         let projectKeys = customerProjects.map(p => p.id.split(' ')[1]);
+        log('(getProjectSuggestion) Finding best matching among [%s] for [%s]', JSON.stringify(projectKeys), projectKey);
         let sm = findBestMatch(projectKey, projectKeys);
         let target = (sm.bestMatch && sm.bestMatch.rating > 0) ? sm.bestMatch.target : null;
         if (!target) return null;
         let suggestion = customerProjects.filter(p => p.id.split(' ')[1] === target.toUpperCase())[0];
-        log('Project [%s] is best match for [%s]', suggestion.id, projectKey);
+        log('(getProjectSuggestion) Project [%s] is best match for [%s]', suggestion.id, projectKey);
         return suggestion;
     } catch (error) {
-        log('Failed to find best match for [%s]', projectKey);
+        log('(getProjectSuggestion) Failed to find best match for [%s]', projectKey);
+        
         return null;
     }
 }
@@ -53,25 +56,31 @@ function findMatch(content, categories) {
  * @param {*} customers 
  */
 function matchEvent(evt, projects, customers) {
-    log('Finding match for [%s]', evt.title);
+    log('(matchEvent) Finding match for [%s]', evt.title);
     let categories = evt.categories.join(' ').toUpperCase();
     let content = [evt.title, evt.body, categories].join(' ').toUpperCase();
     let projectId;
     let match = findMatch(content, categories);
     if (match) {
         projectId = `${match.customerKey} ${match.projectKey}`;
-        log('Found match for [%s]: %s', evt.title, projectId);
+        log('(matchEvent) Found match for [%s]: %s', evt.title, projectId);
         evt.project = _.find(projects, p => p.id === projectId);
         evt.customer = _.find(customers, c => c.id === match.customerKey);
     } else {
-        log('Found match for [%s]', evt.title);
-        evt.project = projects.filter(p => content.indexOf(p.key) !== -1)[0];
-        if (evt.project) {
-            evt.customer = customers.filter(c => c.key === evt.project.key.split(' ')[0])[0];
+        log('(matchEvent) Found no matching tokens for [%s], looking for non-tokenized matches', evt.title);
+        let project = projects.filter(p => content.indexOf(p.id) !== -1)[0];
+        if (project) {
+            log('(matchEvent) Found non-tokenized match in event [%s]: [%s]', evt.title, project.id);
+            evt.project = project;
+            if (evt.project) {
+                log('(matchEvent) Setting customer for event [%s] based on non-tokenized match', evt.title, project.id);
+                evt.customer = customers.filter(c => c.key === evt.project.key.split(' ')[0])[0];
+            }
         }
         match = {};
     }
     if (evt.customer && !evt.project) {
+        log('(matchEvent) Found match for customer [%s] for [%s], but not for any project', evt.customer.name, evt.title);
         let suggestedProject = getProjectSuggestion(projects, evt.customer, match.projectKey);
         if (suggestedProject) evt.suggestedProject = suggestedProject;
     }
