b9e87f89ee8e2b950f4c5bf1c586ea28917c8989
olemp <olemp@puzzlepart.com>
2020-02-06 16:05:46 +0100

    Initial commit

--- middleware/passport/onVerifySignin.js
+++ middleware/passport/onVerifySignin.js
@@ -1,10 +1,19 @@
 const StorageService = require('../../services/storage');
 
 async function onVerifySignin(_iss, _sub, profile, _accessToken, _refreshToken, params, done) {
-    if (!profile.oid) return done(new Error('No OID found in user profile.'), null);
+    if (!profile.oid) {
+        let error = new Error();
+        error.name = 'NO_OID_FOUND';
+        return done(error, null);
+    }
     try {
         const sub = await new StorageService(profile._json.tid).getSubscription();
-        if (!sub) return done(new Error('No access'), null);
+        if (!sub) {
+            let error = new Error();
+            error.name = 'TENANT_NOT_ENROLLED';
+            error.message = 'Your company is not enrolled in Did 365. Please contact did@puzzlepart.com.';
+            return done(error, null);
+        }
         profile['email'] = profile._json.preferred_username;
         profile['sub'] = sub.name;
     } catch (err) {
