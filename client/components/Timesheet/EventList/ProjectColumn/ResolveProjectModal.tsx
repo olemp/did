22d56bedbd2ef8fa5e7e50f2050d8fb10f134f6e
Ole Martin Pettersen <olemp@puzzlepart.com>
2020-02-25 13:18:42 +0100

    Making user messages in ResolveProjectModal non-dismissable (#128)

--- client/components/Timesheet/EventList/ProjectColumn/ResolveProjectModal.tsx
+++ client/components/Timesheet/EventList/ProjectColumn/ResolveProjectModal.tsx
@@ -11,10 +11,6 @@ import * as React from 'react';
 import { useState } from 'react';
 import { SearchProject } from './SearchProject';
 
-const SUGGESTED_PROJECT_MSG_ID = getId('suggested-project-msg');
-const CUSTOMER_MATCH_MSG_ID = getId('customer-match-msg');
-const INFO_MSG_ID = getId('info-msg');
-
 interface IResolveProjectModalProps {
     event: ITimeEntry;
     isOpen?: boolean;
@@ -24,48 +20,30 @@ interface IResolveProjectModalProps {
 
 /**
  * @component ResolveProjectModal
- * @description @todo
+ * @description Modal for resolving a project that hasn't been matched to a project
  */
 export const ResolveProjectModal = ({ isOpen, onDismiss, onProjectSelected, event }: IResolveProjectModalProps) => {
-    const [dismissed, dismiss] = useState<TypedHash<boolean>>({});
     const [scope, setScope] = useState<boolean>(!!event.customer);
-
-    /**
-     * Dismiss message
-     * 
-     * @param {string} id Id of the message
-     */
-    const dismissMessage = (id: string) => {
-        dismiss({ ...dismissed, [id]: true });
-    }
-
     return (
         <Modal
             containerClassName='c-resolveproject-modal'
             isOpen={isOpen}
             onDismiss={onDismiss}>
             <UserMessage
-                id={INFO_MSG_ID}
-                hidden={dismissed[INFO_MSG_ID]}
                 iconName='OutlookLogo'
-                onDismiss={() => dismissMessage(INFO_MSG_ID)}
                 text={`**Please note:** You should try your best to match<a href='${event.webLink}' target='_blank'>the event in Outlook</a>, but in some cases it might be neccessary to manually match.`} />
 
             <UserMessage
-                id={SUGGESTED_PROJECT_MSG_ID}
-                hidden={!event.suggestedProject || dismissed[SUGGESTED_PROJECT_MSG_ID]}
+                hidden={!event.suggestedProject}
                 style={{ marginTop: 5 }}
-                iconName='Lightbulb'
-                onDismiss={() => dismissMessage(SUGGESTED_PROJECT_MSG_ID)} >
+                iconName='Lightbulb' >
                 <p>Did you mean<a href="#" onClick={_ => onProjectSelected(event.suggestedProject)}>{value(event, 'suggestedProject.id', '')}</a>?</p>
             </UserMessage>
 
             <UserMessage
-                id={CUSTOMER_MATCH_MSG_ID}
-                hidden={!event.customer || dismissed[CUSTOMER_MATCH_MSG_ID] || !!event.suggestedProject}
+                hidden={!event.customer || !!event.suggestedProject}
                 style={{ marginTop: 5 }}
-                text={`Event not matched. We found a matching customer \`${value(event, 'customer.name', '')}\`, but not a project with the specified key.`}
-                onDismiss={() => dismissMessage(CUSTOMER_MATCH_MSG_ID)} />
+                text={`Event not matched. We found a matching customer \`${value(event, 'customer.name', '')}\`, but not a project with the specified key.`} />
 
             <Pivot defaultSelectedKey='search-project' styles={{ root: { marginTop: 10 } }}>
                 <PivotItem
