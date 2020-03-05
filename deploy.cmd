483cdd9d52e94b8f7897f58e565a13d71d09b23e
olemp <olemp@puzzlepart.com>
2020-03-05 17:17:17 +0100

    Kudu deployment script runs "npm install" with --force flag #130

--- deploy.cmd
+++ deploy.cmd
@@ -68,7 +68,7 @@ goto :EOF
 echo SYNCHRONIZING DEPLOYMENT SOURCE TO DEPLOYMENT TARGET 
 
 IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
-  call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd;.env.sample;CHANGELOG.md;README.md;.gitignore;.vscode;.github"
+  call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd;.env.sample;CHANGELOG.md;CONTRIBUTING.md;README.md;.gitignore;.vscode;.github"
   IF !ERRORLEVEL! NEQ 0 goto error
 )
 
@@ -78,7 +78,7 @@ echo INSTALLING NPM PACKAGES
 
 IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
   pushd "%DEPLOYMENT_TARGET%"
-  call :ExecuteCmd !NPM_CMD! install --no-progress
+  call :ExecuteCmd !NPM_CMD! install --no-progress --force
   IF !ERRORLEVEL! NEQ 0 goto error
   popd
 )
