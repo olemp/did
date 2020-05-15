3a02eede9b19c49d268dfbdf85e2956c04844058
Carl Joakim Damsleth <kim@damsleth.com>
2020-05-15 15:05:10 +0200

    Cleaned up regions etc in app.js (#365)

--- app.js
+++ app.js
@@ -1,81 +1,53 @@
-require('dotenv').config();
-const _ = require('underscore');
-const log = require('debug')('app');
-const createError = require('http-errors');
-const express = require('express');
-const favicon = require('express-favicon');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const flash = require('connect-flash');
-const passport = require('./middleware/passport');
-const serveGzipped = require('./middleware/gzip');
-const isAuthenticated = require('./middleware/passport/isAuthenticated');
-const hbs = require('hbs');
-const app = express();
+require('dotenv').config()
+const createError = require('http-errors')
+const express = require('express')
+const favicon = require('express-favicon')
+const path = require('path')
+const cookieParser = require('cookie-parser')
+const logger = require('morgan')
+const passport = require('./middleware/passport')
+const serveGzipped = require('./middleware/gzip')
+const isAuthenticated = require('./middleware/passport/isAuthenticated')
+const app = express()
 
-app.use((req, res, next) => {
-    const host = req.get('host');
-    if (host.indexOf('localhost') !== -1 && process.env.AZURE_STORAGE_CONNECTION_STRING.indexOf('dev') === -1) {
-        res.render('error', {
-            error_header: 'Development error',
-            error_message: `Running the server on ${host} requires usage of dev storage.`,
-        });
-    }
-    next();
-});
+app.use(require('./middleware/helmet'))
+app.use(favicon(__dirname + '/public/images/favicon/favicon.ico'))
 
-app.use(require('./middleware/helmet'));
-app.use(favicon(__dirname + '/public/images/favicon/favicon.ico'));
+// Setting up session using connect-azuretables
+app.use(require('./middleware/session'))
 
-//#region Setting up session using connect-azuretables
-app.use(require('./middleware/session'));
-//#endregion
+// HBS views setup
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'hbs')
 
-//#region Flash
-app.use(flash());
-//#endregion
+// API setup
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
 
-//#region HBS views setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-//#endregion
 
-//#region API setup
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+// Passport
+app.use(passport.initialize())
+app.use(passport.session())
 
-// Gzip static assets
-app.get('/*.js', serveGzipped('text/javascript'));
-app.get('/*.css', serveGzipped('text/css'));
+// Routes and middleware
+app.use('/*.js', serveGzipped('text/javascript'))
+app.use(express.static(path.join(__dirname, 'public')))
+app.use('/auth', require('./routes/auth'))
+app.use('/graphql', isAuthenticated, require('./middleware/graphql'))
+app.use('*', require('./routes/index'))
 
-app.use(express.static(path.join(__dirname, 'public')));
-//#endregion
-
-//#region Passport
-app.use(passport.initialize());
-app.use(passport.session());
-//#endregion
-
-//#region Routes/middleware
-app.use('/auth', require('./routes/auth'));
-app.use('/graphql', isAuthenticated, require('./middleware/graphql'));
-app.use('*', require('./routes/index'));
-//#endregion
-
-//#region Error handling
+// Error handling
 app.use((_req, _res, next) => {
-    next(createError(404));
-});
+    next(createError(404))
+})
 
 app.use((error, req, res, _next) => {
-    res.locals.error_header = 'We\'re sorry';
-    res.locals.error_message = error.message;
-    res.status(error.status || 500);
-    res.render('error');
-});
-//#endregion
+    res.locals.error_header = 'We\'re sorry'
+    res.locals.error_message = error.message
+    res.status(error.status || 500)
+    res.render('error')
+})
 
 module.exports = app;
