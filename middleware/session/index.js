9fba58f6c8c745849fdde13c512f2130540fc0a5
Ole Martin Pettersen <olemp@puzzlepart.com>
2020-05-25 13:03:38 +0200

    Changed session timeout to 10080 (1 week in minutes) (#391)

--- middleware/session/index.js
+++ middleware/session/index.js
@@ -2,7 +2,10 @@
 const session = require('express-session')
 const connectAzureTables = require('connect-azuretables')(session)
 
-const store = connectAzureTables.create({ table: 'Sessions', sessionTimeOut: 360 })
+const store = connectAzureTables.create({
+    table: 'Sessions',
+    sessionTimeOut: 10080
+})
 
 module.exports = session({
     store: store,
