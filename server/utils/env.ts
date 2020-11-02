bc0a4f9994cd1c3414797407ae08ab5503b395ff
Ole Martin Pettersen <olemp@puzzlepart.com>
2020-11-02 16:18:04 +0100

    Converted some utils

---
+++ server/utils/env.ts
@@ -0,0 +1,20 @@
+import { isBlank } from 'underscore.string'
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const log = require('debug')('env')
+
+/**
+ * Get environment variable by key with optional fallbackvalue
+ *
+ * @param {string} key Key
+ * @param {any} fallbackValue Fallback vaue if key is not found
+ */
+const getEnvironmentVariable = (key: string, fallbackValue: any) => {
+  const value = process.env[key]
+  if (isBlank(value)) {
+    log('Missing environment variable %s', key)
+    return fallbackValue || null
+  }
+  return value
+}
+
+export default getEnvironmentVariable
---
+++ server/utils/index.ts
@@ -0,0 +1,142 @@
+import moment from 'moment'
+import stripHtml from 'string-strip-html'
+
+
+/**
+ * Strip html from string using string-strip-html
+ *
+ * @param {string} str String
+ */
+export const stripHtmlString = (str: string) => stripHtml(str),
+
+/**
+ * Get duration between two times in hours
+ *
+ * @param {*} startDateTime Start time
+ * @param {*} endDateTime End time
+ */
+export const getDurationHours = (startDateTime, endDateTime) => {
+  return moment.duration(moment(endDateTime).diff(moment(startDateTime))).asHours()
+}
+
+/**
+ * Get period id for the date
+ *
+ * @param {*} date Date
+ */
+export const getPeriod = date => {
+  const d = moment(date)
+  return [d.isoWeek(), d.month() + 1, d.year()].join('_')
+}
+
+/**
+ * Get week for the specified date
+ *
+ * @param {*} date Date
+ */
+export const getWeek = date => {
+  return moment(date).isoWeek()
+}
+
+/**
+ * Get year for the specified date
+ *
+ * @param {*} date Date
+ */
+export const getYear = date => {
+  return moment(date).year()
+},
+
+/**
+ * Get month index for the specified date
+ *
+ * NOTE: Need to add +1 since moment.month is zero-indexed
+ *
+ * @param {*} date Date
+ */
+export const getMonthIndex = date => {
+  return moment(date).month() + 1
+}
+
+/**
+ * Get start of month as string
+ *
+ * @param {*} date Date
+ */
+export const startOfMonth = date => {
+  const d = moment(date).startOf('month')
+  return d.toISOString().replace('Z', '')
+}
+
+/**
+ * Get end of month as string
+ *
+ * @param {*} date Date
+ */
+export const endOfMonth = date => {
+  const d = moment(date).endOf('month')
+  return d.toISOString().replace('Z', '')
+}
+
+/**
+ * Get start of week
+ *
+ * @param {*} week Week number
+ */
+export const startOfWeek = week => {
+  return moment().week(week).startOf('isoWeek')
+}
+
+/**
+ * Get end of week
+ *
+ * @param {*} week Week number
+ */
+export const endOfWeek = week => {
+  return moment().week(week).endOf('isoWeek')
+}
+
+/**
+ * Format date
+ *
+ * @param {*} date Date
+ * @param {*} dateFormat Date format
+ * @param {*} locale Locale
+ */
+export const formatDate = (date, dateFormat, locale, timeZone = 'Europe/Oslo') => {
+  return moment(date).locale(locale).tz(timeZone).format(dateFormat)
+}
+
+/**
+ * Is after today
+ *
+ * @param {*} date Date
+ */
+export const isAfterToday = date => {
+  return moment(date).isAfter(moment())
+}
+
+/**
+ * Converts a string to an array
+ *
+ * @param {*} str String
+ * @param {*} separator String separator
+ */
+export const toArray = (str, separator = '|') => {
+  return (str || '').split(separator).filter(p => p)
+}
+
+/**
+ * Generate int
+ *
+ * @param {string} str String
+ * @param {number} mod Modulator
+ */
+export const generateInt = (str: string, mod: number) =>{
+  return (
+    str
+      .split('')
+      .map(c => c.charCodeAt(0))
+      .reduce((a, b) => a + b) % mod
+  )
+}
