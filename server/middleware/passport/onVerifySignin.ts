a0c4ff9535659bc1fc330536ca8478f2ec0fd024
Ole Martin Pettersen <olemp@puzzlepart.com>
2020-11-03 14:34:57 +0100

    try-catch onVerifySignin

--- server/middleware/passport/onVerifySignin.ts
+++ server/middleware/passport/onVerifySignin.ts
@@ -7,31 +7,35 @@ import { NO_OID_FOUND, TENANT_NOT_ENROLLED, USER_NOT_ENROLLED } from './errors'
 /**
  * On verify signin
  *
- * @param _iss
- * @param _sub
- * @param profile
- * @param _accessToken
- * @param _refreshToken
- * @param oauthToken
- * @param done
+ * @param {any} _iss
+ * @param {any} _sub
+ * @param {any} profile
+ * @param {any} _accessToken
+ * @param {any} _refreshToken
+ * @param {any} oauthToken
+ * @param  done
  */
-export default async function (_iss, _sub, profile, _accessToken, _refreshToken, oauthToken, done) {
-  if (!profile.oid) {
-    debug('No oid found. Returning error NO_OID_FOUND.')
-    return done(NO_OID_FOUND, null)
+export default async function (_iss: any, _sub: any, profile: any, _accessToken: any, _refreshToken: any, oauthToken: any, done) {
+  try {
+    if (!profile.oid) {
+      debug('No oid found. Returning error NO_OID_FOUND.')
+      return done(NO_OID_FOUND, null)
+    }
+    const subscription = await new SubscriptionService().getSubscription(profile._json.tid)
+    if (!subscription) {
+      debug('No subscription found for %s', profile._json.tid)
+      return done(TENANT_NOT_ENROLLED, null)
+    }
+    debug('Subscription found for %s', profile._json.tid)
+    const user = await new AzStorageService(subscription).getUser(profile.oid)
+    if (!user) {
+      debug('User %s is not registered for %s', profile.oid, subscription.name)
+      return done(USER_NOT_ENROLLED, null)
+    }
+    user.subscription = subscription
+    user.oauthToken = oauthToken
+    return done(null, user)
+  } catch (error) {
+    debug(error)
   }
-  const subscription = await new SubscriptionService().getSubscription(profile._json.tid)
-  if (!subscription) {
-    debug('No subscription found for %s', profile._json.tid)
-    return done(TENANT_NOT_ENROLLED, null)
-  }
-  debug('Subscription found for %s', profile._json.tid)
-  const user = await new AzStorageService(subscription).getUser(profile.oid)
-  if (!user) {
-    debug('User %s is not registered for %s', profile.oid, subscription.name)
-    return done(USER_NOT_ENROLLED, null)
-  }
-  user.subscription = subscription
-  user.oauthToken = oauthToken
-  return done(null, user)
 }
