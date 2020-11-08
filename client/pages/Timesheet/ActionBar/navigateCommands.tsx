1a85873c391137d9f460802a96234c49140d78cd
Ole Martin Pettersen <olemp@puzzlepart.com>
2020-11-08 01:43:50 +0100

    Navigate commands disabled when loading

--- client/pages/Timesheet/ActionBar/navigateCommands.tsx
+++ client/pages/Timesheet/ActionBar/navigateCommands.tsx
@@ -2,28 +2,30 @@ import { IContextualMenuItem } from 'office-ui-fabric-react/lib/ContextualMenu'
 import { ITimesheetContext } from '../context'
 import { ACTIONBAR_ICON_PROPS } from './ACTIONBAR_ICON_PROPS'
 
-const currentWeek = ({ scope, dispatch, t }: ITimesheetContext): IContextualMenuItem => ({
+const currentWeek = ({ loading, scope, dispatch, t }: ITimesheetContext): IContextualMenuItem => ({
     key: 'GO_TO_CURRENT_WEEK_COMMAND',
     iconOnly: true,
     iconProps: { iconName: 'RenewalCurrent', ...ACTIONBAR_ICON_PROPS },
     onClick: () => dispatch({ type: 'MOVE_SCOPE', payload: new Date().toISOString() }),
-    disabled: scope.isCurrentWeek,
+    disabled: scope.isCurrentWeek || !!loading,
     title: t('timesheet.goToCurrentWeek'),
 })
 
-const prevWeek = ({ dispatch, t }: ITimesheetContext): IContextualMenuItem => ({
+const prevWeek = ({ loading, dispatch, t }: ITimesheetContext): IContextualMenuItem => ({
     key: 'GO_TO_PREV_WEEK_COMMAND',
     iconOnly: true,
     iconProps: { iconName: 'Back', ...ACTIONBAR_ICON_PROPS },
     onClick: () => dispatch({ type: 'MOVE_SCOPE', payload: { amount: -1, unit: 'week' } }),
+    disabled: !!loading,
     title: t('timesheet.goToPrevWeek')
 })
 
-const nextWeek = ({ dispatch, t }: ITimesheetContext): IContextualMenuItem => ({
+const nextWeek = ({ loading, dispatch, t }: ITimesheetContext): IContextualMenuItem => ({
     key: 'GO_TO_NEXT_WEEK_COMMAND',
     iconOnly: true,
     iconProps: { iconName: 'Forward', ...ACTIONBAR_ICON_PROPS },
     onClick: () => dispatch({ type: 'MOVE_SCOPE', payload: { amount: 1, unit: 'week' } }),
+    disabled: !!loading,
     title: t('timesheet.goToNextWeek'),
 })
 
