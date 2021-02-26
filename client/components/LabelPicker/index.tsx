d988a6740d5df35cf091d27ce58562116fb8d1ba
Ole Martin Pettersen <olemp@puzzlepart.com>
2021-02-26 21:57:35 +0100

    Fix for selection in LabelPicker using hook useLabelPicker (#836)

--- client/components/LabelPicker/index.tsx
+++ client/components/LabelPicker/index.tsx
@@ -1,52 +1,26 @@
-import { useQuery } from '@apollo/client'
 import { EntityLabel } from 'components/EntityLabel'
 import { Icon, Label } from 'office-ui-fabric'
-import React, { FunctionComponent, useEffect, useRef, useState } from 'react'
+import React, { FunctionComponent, useRef } from 'react'
 import { useTranslation } from 'react-i18next'
-import { LabelObject } from 'types'
-import { omit } from 'underscore'
 import styles from './LabelPicker.module.scss'
-import $labels from './labels.gql'
 import { SelectCallout } from './SelectCallout'
 import { ILabelPickerProps } from './types'
+import { useLabelPicker } from './useLabelPicker'
 
 export const LabelPicker: FunctionComponent<ILabelPickerProps> = (props: ILabelPickerProps) => {
   const { t } = useTranslation()
-  const { data } = useQuery($labels, { fetchPolicy: 'cache-and-network' })
+  const {
+    labels,
+    selectedLabels,
+    showCallout,
+    setShowCallout,
+    onToggleLabel
+  } = useLabelPicker(props)
   const toggleRef = useRef()
-  const [labels, setLabels] = useState<LabelObject[]>([])
-  const [selectedLabels, setSelectedLabels] = useState<LabelObject[]>([])
-  const [showCallout, setShowCallout] = useState<boolean>(false)
-
-  function onToggleLabel(label: LabelObject) {
-    const _selectedLabels = [...selectedLabels]
-    const index = _selectedLabels.indexOf(label)
-    if (index === -1) {
-      _selectedLabels.push(label)
-      setSelectedLabels(_selectedLabels)
-    } else {
-      _selectedLabels.splice(index, 1)
-      setSelectedLabels(_selectedLabels)
-    }
-    props.onChange(_selectedLabels)
-  }
-
-  useEffect(() => {
-    if (data?.labels) {
-      const _labels: LabelObject[] = data.labels.map((lbl: any) => omit(lbl, '__typename'))
-      setLabels(_labels)
-      if (props.defaultSelectedKeys) {
-        const _selectedLabels = _labels.filter(
-          (lbl) => props.defaultSelectedKeys.indexOf(lbl.name) !== -1
-        )
-        setSelectedLabels(_selectedLabels)
-      }
-    }
-  }, [data, props.defaultSelectedKeys])
 
   return (
     <div className={`${styles.root} ${props.className}`}>
-      <Label className={styles.label}>
+      <Label className={styles.inputLabel}>
         <span>{props.label}</span>
         <span
           className={styles.toggleIcon}
@@ -55,9 +29,11 @@ export const LabelPicker: FunctionComponent<ILabelPickerProps> = (props: ILabelP
           <Icon iconName='Settings' />
         </span>
       </Label>
+      <div className={styles.selectedLabels}>
       {selectedLabels.map((lbl) => (
         <EntityLabel key={lbl.name} label={lbl} />
       ))}
+      </div>
       <span className={styles.noneSelected} hidden={selectedLabels.length > 0}>
         {t('common.noneSelectedMessage')}
       </span>
---
+++ client/components/LabelPicker/useLabelPicker.tsx
@@ -0,0 +1,48 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useQuery } from '@apollo/client'
+import { useEffect, useState } from 'react'
+import { LabelObject } from 'types'
+import { omit } from 'underscore'
+import $labels from './labels.gql'
+import { ILabelPickerProps } from './types'
+
+export function useLabelPicker({ onChange, defaultSelectedKeys }: ILabelPickerProps) {
+  const { data } = useQuery($labels, { fetchPolicy: 'cache-first' })
+  const [labels, setLabels] = useState<LabelObject[]>([])
+  const [selectedLabels, setSelectedLabels] = useState<LabelObject[]>([])
+  const [showCallout, setShowCallout] = useState<boolean>(false)
+
+  function onToggleLabel(label: LabelObject) {
+    const _selectedLabels = [...selectedLabels]
+    const index = _selectedLabels.indexOf(label)
+    if (index === -1) {
+      _selectedLabels.push(label)
+      setSelectedLabels(_selectedLabels)
+    } else {
+      _selectedLabels.splice(index, 1)
+      setSelectedLabels(_selectedLabels)
+    }
+    onChange(_selectedLabels)
+  }
+
+  useEffect(() => {
+    if (data?.labels) {
+      const _labels: LabelObject[] = data.labels.map((lbl: any) => omit(lbl, '__typename'))
+      setLabels(_labels)
+      if (defaultSelectedKeys) {
+        const _selectedLabels = _labels.filter(
+          (lbl) => defaultSelectedKeys.indexOf(lbl.name) !== -1
+        )
+        setSelectedLabels(_selectedLabels)
+      }
+    }
+  }, [data])
+
+  return {
+    onToggleLabel,
+    showCallout,
+    setShowCallout,
+    labels,
+    selectedLabels
+  }
+}
