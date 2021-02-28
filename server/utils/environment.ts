13e23413ae2e7d4f1ce1ee8642ac012b52d98cc6
Ole Martin Pettersen <olemp@puzzlepart.com>
2021-02-28 14:52:10 +0100

    Minor adjustment to debugging in environment util

--- server/utils/environment.ts
+++ server/utils/environment.ts
@@ -1,9 +1,11 @@
 import createDebug from 'debug'
 import { isBlank } from 'underscore.string'
-const debug = createDebug('env')
+const debug = createDebug('environment')
 
 /**
  * Get environment variable by key with optional fallbackvalue
+ * 
+ * Makes it easier to work with process.env.
  *
  * @param key - Key
  * @param fallbackValue - Fallback vaue if key is not found
@@ -11,7 +13,7 @@ const debug = createDebug('env')
 export function environment(key: string, fallbackValue: string = null) {
   const value = process.env[key]
   if (isBlank(value)) {
-    debug('Missing environment variable %s', key)
+    debug('Missing environment variable %s. Using %s instead.', key, fallbackValue)
     return fallbackValue || null
   }
   return value
