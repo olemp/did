5375765c37498b175a70a484b45f84aa549b3e14
Ole Martin Pettersen <olemp@puzzlepart.com>
2021-03-28 22:33:26 +0200

    build: minor refactor in webpack config 🏗️

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,6 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const webpack = require('webpack')
 const debug = require('debug')('webpack')
-const { CustomCompileHooks } = require('./webpack-compile-hooks')
 
 const {
   MODE,
@@ -53,6 +52,7 @@ function getPluginsForEnvironment() {
   if (IS_DEVELOPMENT) {
     const LiveReloadPlugin = tryRequire('webpack-livereload-plugin')
     const WebpackBuildNotifierPlugin = tryRequire('webpack-build-notifier')
+    const { CustomCompileHooks } = require('./webpack-compile-hooks')
     plugins.push(
       new CustomCompileHooks({
         url: new URL(process.env.MICROSOFT_REDIRECT_URI).origin,
@@ -81,12 +81,6 @@ function getOptimizationForEnvironment() {
     // https://medium.com/webpack/webpack-4-code-splitting-chunk-graph-and-the-splitchunks-optimization-be739a861366
     splitChunks: {
       chunks: 'all'
-    },
-    // Keep the runtime chunk separated to enable long term caching
-    // https://twitter.com/wSokra/status/969679223278505985
-    // https://github.com/facebook/create-react-app/issues/5358
-    runtimeChunk: {
-      name: entrypoint => `runtime-${entrypoint.name}`,
     },
     minimize: false,
     minimizer: [],
