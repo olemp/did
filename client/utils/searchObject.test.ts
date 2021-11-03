1a2fd9b381f6846bd5b946fc5b8447279411eb09
Ole Martin Pettersen <olemp@puzzlepart.com>
2021-11-03 14:50:59 +0100

    Upgrading to node v14.18.1 (#1007)

--- client/utils/searchObject.test.ts
+++ client/utils/searchObject.test.ts
@@ -1,3 +1,16 @@
 import test from 'ava'
+import { searchObject } from './searchObject'
 
-test.todo('Implement test for this later')
+test('searchObject searches the specified item for the specified searchTerm', (t) => {
+  const result = searchObject({
+    item: { name: 'Ole Martin', __typename: 'User' },
+    searchTerm: 'Ole'
+  })
+  const result2 = searchObject({
+    item: { name: 'Ole Martin', age: 33, __typename: 'User' },
+    searchTerm: 'Ole',
+    pick_: ['age']
+  })
+  t.is(result, true)
+  t.is(result2, false)
+})
