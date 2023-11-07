2ed7ce75cdb61bbf31f21d138e2a21126880211a
Ole Martin Pettersen <olemp@puzzlepart.com>
2023-11-07 11:06:11 +0100

    fix: fixed an issue with checked state in multicheckboxfield 🐛 (closes #1160)

--- client/pages/Admin/SubscriptionSettings/context.ts
+++ client/pages/Admin/SubscriptionSettings/context.ts
@@ -7,10 +7,15 @@ export interface ISubscriptionContext {
    */
   settings: SubscriptionSettings
 
-  /***
-   * A callback function to be called when a setting is changed.
+  /**
+   * A callback function to be called when a setting is changed. The key is a
+   * `string` while the value can be a `boolean`, `string`, or a function that
+   * takes the current value and returns a new value.
    */
-  onChange?: (key: string, value: any) => void
+  onChange?: (
+    key: string,
+    value: boolean | string | ((value: any) => any)
+  ) => void
 }
 
 export const SubscriptionContext = createContext<ISubscriptionContext>(null)
