dbe4d2edc6f8afba8fc132b41d0c2a026f2fb46e
Ole Martin Pettersen <olemp@puzzlepart.com>
2023-11-07 11:12:24 +0100

    fix: fixed an issue with invalid graphql query in useupdateuserconfiguration 🐛 (closes #1162)

--- client/hooks/user/useUpdateUserConfiguration.ts
+++ client/hooks/user/useUpdateUserConfiguration.ts
@@ -1,41 +1,16 @@
 /* eslint-disable unicorn/prevent-abbreviations */
 import { useMutation } from '@apollo/client'
 import { TypedMap } from 'hooks'
-import { useCallback, useEffect } from 'react'
+import { useCallback } from 'react'
 import $updateUserConfiguration from './update-user-configuration.gql'
-
-export type UseUpdateUserConfigurationParamType<T = any> = {
-  config?: T
-  autoUpdate?: boolean
-}
-
-export type UseUpdateUserConfigurationReturnType = {
-  updateLastActive?: () => Promise<void>
-  updateUserSettings?: (user: TypedMap<any, any, any>) => Promise<boolean>
-}
-
 /**
- * Update user configuration hook
- *
- * Retrieves config JSON and update (boolean) and uses useMutation.
- * It will only execute the mutation if update is equal to true, and
- * the value has changed.
- *
- * If `autoUpdate` is set to true, the mutation is ran on every
- * change to the specifie `config` using `useEffect`
- *
- * @remarks For now this is how we update user configuration,
- * but it might be better ways. For now this should do.
- *
- * @param params Parameters
+ * Update user configuration hook.
+ 
  *
  * @category React Hook
  */
-export function useUpdateUserConfiguration(
-  params?: UseUpdateUserConfigurationParamType
-): UseUpdateUserConfigurationReturnType {
+export function useUpdateUserConfiguration() {
   const [updateUserConfiguration] = useMutation($updateUserConfiguration)
-  const stringValue = JSON.stringify(params?.config ?? {})
 
   const updateLastActive = useCallback(async () => {
     await updateUserConfiguration({
@@ -56,14 +31,6 @@ export function useUpdateUserConfiguration(
     },
     []
   )
-
-  useEffect(() => {
-    if (params?.autoUpdate) {
-      updateUserConfiguration({
-        variables: { configuration: stringValue }
-      })
-    }
-  }, [stringValue])
 
   return {
     updateUserSettings,
