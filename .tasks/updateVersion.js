1f7d6725c13ef3a40932b2358468d3f299302db5
Ole Martin Pettersen <olemp@puzzlepart.com>
2024-02-21 15:37:09 +0100

    fixed broken script updateVersion.js

--- .tasks/updateVersion.js
+++ .tasks/updateVersion.js
@@ -4,11 +4,80 @@ const colors = require('colors/safe')
 const exec = require('child_process').exec
 const package = require('../package.json')
 const { promisify } = require('util')
+const { parse } = require('path')
 const writeFileAsync = promisify(fs.writeFile)
 const execAsync = promisify(exec)
 
-const currentVersion = package.version
-let newVersion = currentVersion
+class Version {
+    constructor(versionString) {
+        const [major, minor, patch] = versionString.split('-')[0].split('.')
+        this.major = parseInt(major)
+        this.minor = parseInt(minor)
+        this.patch = parseInt(patch)
+        this.build = this._parseBuild(versionString)
+    }
+
+    _parseBuild(versionString) {
+        const build = versionString.split('-')[1]
+        if (!build) return null
+        const [type, number] = build.split('.')
+        return {
+            type,
+            number: number !== undefined ? parseInt(number) : 0
+        }
+    }
+
+    incrementMajor() {
+        this.major += 1
+        this.minor = 0
+        this.patch = 0
+        this.build = {}
+        return this
+    }
+
+    incrementMinor() {
+        this.minor += 1
+        this.patch = 0
+        this.build = {}
+        return this
+    }
+
+    incrementPatch() {
+        this.patch += 1
+        this.build = {}
+        return this
+    }
+
+    incrementAlpha() {
+        this.build = {
+            type: 'alpha',
+            number: this.isAlpha ? this.build.number + 1 : 0
+        }
+        return this
+    }
+
+    incrementBeta() {
+        this.build.type = 'beta'
+        this.build.number = this.isBeta ? this.build.number + 1 : 0
+        return this
+    }
+
+    get isAlpha() {
+        return this.build.type === 'alpha'
+    }
+
+    get isBeta() {
+        return this.build.type === 'beta'
+    }
+
+    toString() {
+        return `${this.major}.${this.minor}.${this.patch}${this.build.type ? `-${this.build.type}.${this.build.number}` : ''}`
+    }
+}
+
+
+const currentVersion = new Version(package.version)
+let newVersion = new Version(package.version)
 
 /**
  * Updates the version of the package based on the provided arguments.
@@ -17,42 +86,32 @@ let newVersion = currentVersion
  */
 async function updateVersion({ major, minor, patch, alpha, beta }) {
     if (argv.major) {
-        newVersion = currentVersion.split('.').map((v, i) => i === 0 ? parseInt(v) + 1 : 0).splice(0, 3).join('.')
+        newVersion = newVersion.incrementMajor()
     } else if (argv.minor) {
-        newVersion = currentVersion.split('.').map((v, i) => i === 1 ? parseInt(v) + 1 : 0).splice(0, 3).join('.')
+        newVersion = newVersion.incrementMinor()
     } else if (argv.patch) {
-        newVersion = currentVersion.split('.').map((v, i) => i === 2 ? parseInt(v) + 1 : 0).splice(0, 3).join('.')
+        newVersion = newVersion.incrementPatch()
     }
     else if (argv.alpha) {
-        if (currentVersion.includes('-alpha')) {
-            const alphaVersion = parseInt(currentVersion.split('-alpha.')[1])
-            newVersion = currentVersion.split('-alpha.')[0] + '-alpha.' + (alphaVersion + 1)
-        } else {
-            newVersion = currentVersion + '-alpha.0'
-        }
+        newVersion = newVersion.incrementAlpha()
     } else if (argv.beta) {
-        if (currentVersion.includes('-beta')) {
-            const alphaVersion = parseInt(currentVersion.split('-beta.')[1])
-            newVersion = currentVersion.split('-beta.')[0] + '-beta.' + (alphaVersion + 1)
-        } else {
-            newVersion = currentVersion + '-beta.0'
-        }
+        newVersion = newVersion.incrementBeta()
     }
-
-    package.version = newVersion
-    console.log(`Updating version from ${colors.cyan(currentVersion)} to ${colors.magenta(newVersion)} in package.json`)
+    package.version = newVersion.toString()
+    console.log(`Updating version from ${colors.cyan(currentVersion.toString())} to ${colors.magenta(package.version)} in package.json`)
+    if (argv.dryRun) return
     await writeFileAsync('./package.json', JSON.stringify(package, null, 2))
     await execAsync('npm install')
     if (argv.push) {
-        console.log(`Updated version from ${colors.cyan(currentVersion)} to ${colors.magenta(newVersion)}. ${colors.magenta('Committing')} and ${colors.magenta('pushing')} changes to the repository.`)
+        console.log(`Updated version from ${colors.cyan(currentVersion.toString())} to ${colors.magenta(package.version)}. ${colors.magenta('Committing')} and ${colors.magenta('pushing')} changes to the repository.`)
         await execAsync('git add --all')
-        await execAsync(`git commit -m "Updated version to ${newVersion} [skip-ci]"`)
+        await execAsync(`git commit -m "Updated version to ${package.version} [skip-ci]"`)
         await execAsync('git push')
-        await execAsync(`git tag -a v${newVersion} -m "${newVersion}"`)
+        await execAsync(`git tag -a v${package.version} -m "${package.version}"`)
         await execAsync('git push --tags');
-        console.log(`Updated version from ${colors.cyan(currentVersion)} to ${colors.magenta(newVersion)}. New tag have been created and pushed to the repository.`)
+        console.log(`Updated version from ${colors.cyan(currentVersion.toString())} to ${colors.magenta(package.version)}. New tag have been created and pushed to the repository.`)
     } else {
-        console.log(`Updated version from ${colors.cyan(currentVersion)} to ${colors.magenta(newVersion)}. Remember to ${colors.magenta('commit')} and ${colors.magenta('push')} the changes.`)
+        console.log(`Updated version from ${colors.cyan(currentVersion.toString())} to ${colors.magenta(package.version)}. Remember to ${colors.magenta('commit')} and ${colors.magenta('push')} the changes.`)
     }
 }
 
