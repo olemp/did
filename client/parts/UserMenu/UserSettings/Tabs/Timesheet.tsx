ec755807ddbec0e1500a1b4d66b0a26d9460ca52
github-automatic-chores <github-automatic-chores@users.noreply.github.com>
2024-07-11 11:32:59 +0000

    automatic chores (f50eefeb) 📝 [skip-ci]
    This is a commit done by the GitHub action https://github.com/Puzzlepart/did/blob/dev/.github/workflows/automatic_chores.yml. This way we avoid having to generate documentation, run eslint etc. locally before commiting to the repo. See https://github.com/Puzzlepart/did/issues/903 for more details.

--- client/parts/UserMenu/UserSettings/Tabs/Timesheet.tsx
+++ client/parts/UserMenu/UserSettings/Tabs/Timesheet.tsx
@@ -6,21 +6,25 @@ import { UserSettingsTabComponent } from './types'
 import styles from './UserSettingsTabComponent.module.scss'
 
 export const Timesheet: UserSettingsTabComponent = ({ register, model }) => {
-    const { t } = useTranslation()
-    return (
-        <div className={styles.container}>
-            <InputControl
-                {...register(CONFIG_KEYS.workWeekHours)}
-                label={t('common.workWeekHoursLabel')}
-                description={t('common.workWeekHoursDescription')}
-                type='number'
-                contentAfter={<div>{((model.value(CONFIG_KEYS.workWeekHours) / 40)*100).toFixed(1)}%</div>}
-            />
-            <CheckboxControl
-                {...register(CONFIG_KEYS.timesheetRoundUpEvents)}
-                label={t('common.roundUpEventsLabel')}
-                description={t('common.roundUpEventsDescription')}
-            />
-        </div>
-    )
+  const { t } = useTranslation()
+  return (
+    <div className={styles.container}>
+      <InputControl
+        {...register(CONFIG_KEYS.workWeekHours)}
+        label={t('common.workWeekHoursLabel')}
+        description={t('common.workWeekHoursDescription')}
+        type='number'
+        contentAfter={
+          <div>
+            {((model.value(CONFIG_KEYS.workWeekHours) / 40) * 100).toFixed(1)}%
+          </div>
+        }
+      />
+      <CheckboxControl
+        {...register(CONFIG_KEYS.timesheetRoundUpEvents)}
+        label={t('common.roundUpEventsLabel')}
+        description={t('common.roundUpEventsDescription')}
+      />
+    </div>
+  )
 }
