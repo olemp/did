e866d68f36a22d5866d50671273affcadd5c301c
github-automatic-chores <github-automatic-chores@users.noreply.github.com>
2024-08-05 12:19:29 +0000

    automatic chores (a271f609) 📝 [skip-ci]
    This is a commit done by the GitHub action https://github.com/Puzzlepart/did/blob/dev/.github/workflows/automatic_chores.yml. This way we avoid having to generate documentation, run eslint etc. locally before commiting to the repo. See https://github.com/Puzzlepart/did/issues/903 for more details.

--- shared/utils/toFixed.ts
+++ shared/utils/toFixed.ts
@@ -3,9 +3,9 @@
  *
  * @param value - The number to be rounded.
  * @param precision - The number of decimal places to round to. Default is 2.
- * 
+ *
  * @returns The rounded number.
  */
 export const toFixed = (value: number, precision = 2) => {
-    return Number.parseFloat(value.toFixed(precision))
+  return Number.parseFloat(value.toFixed(precision))
 }
