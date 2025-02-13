833a8c4559f85a97a38b32f2bb4a60600ee2b01a
Ole Martin Pettersen <olemp@puzzlepart.com>
2025-02-13 11:46:46 +0100

    fix for error in job run 37153987394

--- client/parts/UserMenu/UserSettings/Tabs/types.ts
+++ client/parts/UserMenu/UserSettings/Tabs/types.ts
@@ -3,6 +3,6 @@ import { useUserSettings } from '../useUserSettings'
 
 export interface UserSettingsTabComponent
   extends React.FC<{
-    register: ReturnType<typeof useUserSettings>['register']
-    model: ReturnType<typeof useUserSettings>['model']
-  }> {}
+    register: ReturnType<typeof useUserSettings>['formControlProps']['register']
+    model: ReturnType<typeof useUserSettings>['formControlProps']['model']
+  }> { }
