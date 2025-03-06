f72c73a7442d0251dadd4f462afb2744d2e66d36
Ole Martin Pettersen <olemp@puzzlepart.com>
2025-03-06 14:56:24 +0100

    refactor: extract projectlist props creation to a reusable function ♻️

--- client/pages/Projects/Projects.tsx
+++ client/pages/Projects/Projects.tsx
@@ -1,10 +1,9 @@
 import { Tabs } from 'components/Tabs'
 import React, { FC } from 'react'
-import { useTranslation } from 'react-i18next'
 import { ProjectsContext } from './context'
 import { ProjectDetails } from './ProjectDetails'
 import { ProjectForm } from './ProjectForm'
-import { IProjectListProps, ProjectList } from './ProjectList'
+import { ProjectList } from './ProjectList'
 import { CLOSE_EDIT_PANEL } from './reducer'
 import { useProjects } from './useProjects'
 
@@ -12,8 +11,7 @@ import { useProjects } from './useProjects'
  * @category Function Component
  */
 export const Projects: FC = () => {
-  const { t } = useTranslation()
-  const { context, renderDetails, defaultTab } = useProjects()
+  const { t, context, renderDetails, defaultTab, createListProps } = useProjects()
 
   return (
     <ProjectsContext.Provider value={{ ...context }}>
@@ -26,34 +24,12 @@ export const Projects: FC = () => {
             s: [
               ProjectList,
               t('common.search'),
-              {
-                getKey: (item) => item?.tag,
-                enableShimmer: context.loading,
-                searchBox: {
-                  persist: true,
-                  disabled: context.loading,
-                  placeholder: (state) =>
-                    t('projects.searchPlaceholder', {
-                      count: state.origItems?.length ?? 0
-                    })
-                }
-              } as IProjectListProps
+              createListProps('s')
             ],
             m: [
               ProjectList,
               t('projects.myProjectsText'),
-              {
-                getKey: (item) => item?.tag,
-                enableShimmer: context.loading,
-                searchBox: {
-                  persist: true,
-                  disabled: context.loading,
-                  placeholder: (state) =>
-                    t('projects.myProjectsSearchPlaceholder', {
-                      count: state.origItems?.length ?? 0
-                    })
-                }
-              } as IProjectListProps
+              createListProps('m')
             ],
             new: [ProjectForm, t('projects.createNewText')]
           }}
