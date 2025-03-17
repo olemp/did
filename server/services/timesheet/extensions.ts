bed103b3f19bcdb19079b28ef805d034dd6fefeb
github-automatic-chores <github-automatic-chores@users.noreply.github.com>
2025-03-17 15:51:56 +0000

    automatic chores (3e14df37) 📝 [skip-ci]
    This is a commit done by the GitHub action https://github.com/Puzzlepart/did/blob/dev/.github/workflows/automatic_chores.yml. This way we avoid having to generate documentation, run eslint etc. locally before commiting to the repo. See https://github.com/Puzzlepart/did/issues/903 for more details.

--- server/services/timesheet/extensions.ts
+++ server/services/timesheet/extensions.ts
@@ -2,32 +2,35 @@ import { ClientEventInput, EventObject, Project } from '../../graphql'
 import { ITimesheetPeriodData } from './types'
 import _ from 'lodash'
 import { tryParseJson } from '../../utils'
-import { ProjectResourcesExtensionId, ProjectRoleDefinitionsExtensionId } from '../mongo/project'
+import {
+  ProjectResourcesExtensionId,
+  ProjectRoleDefinitionsExtensionId
+} from '../mongo/project'
 
 /**
  * Context object passed to extensions with all the data they might need
  */
 export type TimeEntryExtensionContext = {
-    period: ITimesheetPeriodData
-    matchedEvent: ClientEventInput
-    originalEvent: EventObject
-    projects: Project[]
+  period: ITimesheetPeriodData
+  matchedEvent: ClientEventInput
+  originalEvent: EventObject
+  projects: Project[]
 }
 
 /**
  * Interface for time entry extensions
  */
 export interface TimeEntryExtension {
-    apply: (
-        event: ClientEventInput & EventObject,
-        context: TimeEntryExtensionContext
-    ) => Record<string, any>
+  apply: (
+    event: ClientEventInput & EventObject,
+    context: TimeEntryExtensionContext
+  ) => Record<string, any>
 
-    /**
-     * Additional properties or methods for
-     * the extension class.
-     */
-    [key: string]: any
+  /**
+   * Additional properties or methods for
+   * the extension class.
+   */
+  [key: string]: any
 }
 
 /**
@@ -35,62 +38,62 @@ export interface TimeEntryExtension {
  * Adds role properties to events based on project resources and role definitions.
  */
 export const ProjectRoleEventExtension: TimeEntryExtension = {
-    apply(_event, { period, matchedEvent, projects }) {
-        if (!matchedEvent.projectId) return {}
-        const role = this.findProjectRole(projects, matchedEvent.projectId, period.userId)
-        if (!role) return {}
+  apply(_event, { period, matchedEvent, projects }) {
+    if (!matchedEvent.projectId) return {}
+    const role = this.findProjectRole(
+      projects,
+      matchedEvent.projectId,
+      period.userId
+    )
+    if (!role) return {}
 
-        return { role }
-    },
+    return { role }
+  },
 
-    /**
-     * Finds the project role for a given project based on the user ID.
-     * 
-     * @param projects - The projects to search for the role
-     * @param projectId - The project ID
-     * @param userId - The user ID to search for in the project resources
-     * @returns An object containing the project role name and hourly rate if found, otherwise null
-     */
-    findProjectRole(
-        projects: Project[],
-        projectId: string,
-        userId: string
-    ) {
-        const project = _.find(projects, ({ _id }) => _id === projectId)
-        if (!project) return null
+  /**
+   * Finds the project role for a given project based on the user ID.
+   *
+   * @param projects - The projects to search for the role
+   * @param projectId - The project ID
+   * @param userId - The user ID to search for in the project resources
+   * @returns An object containing the project role name and hourly rate if found, otherwise null
+   */
+  findProjectRole(projects: Project[], projectId: string, userId: string) {
+    const project = _.find(projects, ({ _id }) => _id === projectId)
+    if (!project) return null
 
-        const extensions = tryParseJson(
-            _.get(project, 'extensions', { default: 'null' }) as string
-        )
-        if (!extensions) return null
+    const extensions = tryParseJson(
+      _.get(project, 'extensions', { default: 'null' }) as string
+    )
+    if (!extensions) return null
 
-        const resources = _.get(
-            extensions,
-            `${ProjectResourcesExtensionId}.properties.resources`,
-            { default: [] }
-        )
-        const roleDefinitions = _.get(
-            extensions,
-            `${ProjectRoleDefinitionsExtensionId}.properties.roleDefinitions`,
-            { default: [] }
-        )
+    const resources = _.get(
+      extensions,
+      `${ProjectResourcesExtensionId}.properties.resources`,
+      { default: [] }
+    )
+    const roleDefinitions = _.get(
+      extensions,
+      `${ProjectRoleDefinitionsExtensionId}.properties.roleDefinitions`,
+      { default: [] }
+    )
 
-        const defaultRole = _.find(roleDefinitions, ({ isDefault }) => isDefault)
-        const resource = _.find(resources, ({ id }) => id === userId)
+    const defaultRole = _.find(roleDefinitions, ({ isDefault }) => isDefault)
+    const resource = _.find(resources, ({ id }) => id === userId)
 
-        if (!resource) {
-            if (!defaultRole) return null
-            return {
-                name: defaultRole.name,
-                hourlyRate: defaultRole.hourlyRate
-            }
-        }
-
-        return {
-            name: resource.projectRole,
-            hourlyRate: resource.hourlyRate
-        }
+    if (!resource) {
+      if (!defaultRole) return null
+      return {
+        name: defaultRole.name,
+        hourlyRate: defaultRole.hourlyRate
+      }
     }
+
+    return {
+      name: resource.projectRole,
+      hourlyRate: resource.hourlyRate
+    }
+  }
 }
 
 /**
@@ -98,23 +101,24 @@ export const ProjectRoleEventExtension: TimeEntryExtension = {
  * Handles correct precedence of different duration values.
  */
 export const DurationEventExtension: TimeEntryExtension = {
-    apply(_event, { matchedEvent, originalEvent }) {
-        return {
-            startDateTime: matchedEvent.startDateTime ?? originalEvent.startDateTime,
-            endDateTime: matchedEvent.endDateTime ?? originalEvent.endDateTime,
-            duration: matchedEvent.duration ?? originalEvent.duration,
-            originalDuration: matchedEvent.originalDuration ?? originalEvent.originalDuration,
-            adjustedMinutes: matchedEvent.adjustedMinutes ?? originalEvent.adjustedMinutes
-        }
+  apply(_event, { matchedEvent, originalEvent }) {
+    return {
+      startDateTime: matchedEvent.startDateTime ?? originalEvent.startDateTime,
+      endDateTime: matchedEvent.endDateTime ?? originalEvent.endDateTime,
+      duration: matchedEvent.duration ?? originalEvent.duration,
+      originalDuration:
+        matchedEvent.originalDuration ?? originalEvent.originalDuration,
+      adjustedMinutes:
+        matchedEvent.adjustedMinutes ?? originalEvent.adjustedMinutes
     }
+  }
 }
-
 
 /**
  * Registry of all event extensions.
  * Add new extension classes to this array to automatically apply them to events.
  */
 export const eventExtensions = [
-    DurationEventExtension,
-    ProjectRoleEventExtension
+  DurationEventExtension,
+  ProjectRoleEventExtension
 ]
